9f3248352563d5cfa439f630939db020d08b1aac
Leon ROUX <leon.roux@federate.us>
2024-01-30 22:22:18 +0100

    Store the state to a S3 bucket

--- main.tf
+++ main.tf
@@ -1,13 +1,13 @@
-module "k8s" {
-  source = "./k8s"
-
-  address = "zion.internal"
-  port    = "223"
-}
-
 module "ca" {
   source = "./ca"
 
   org = "k3s.zion.internal"
   cn  = "R0"
+}
+
+module "k8s" {
+  source = "./k8s"
+
+  address = "s.unw.re"
+  port    = 223
 }
