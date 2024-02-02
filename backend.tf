ebbfb7b28fcd5b9ccbef834dd084d73581c7b2e6
Leon ROUX <leon.roux@federate.us>
2024-02-02 18:51:19 +0100

    Prepared cluster authentication in the main.tf and added ClusterIssuer

--- backend.tf
+++ backend.tf
@@ -3,7 +3,7 @@ terraform {
     bucket = "otf"
     key    = "infra.state"
 
-    use_path_style            = true
+    use_path_style              = true
     skip_region_validation      = true
     skip_metadata_api_check     = true
     skip_requesting_account_id  = true
--- main.tf
+++ main.tf
@@ -1,9 +1,3 @@
-module "ca" {
-  source = "./ca"
-
-  org = "k3s.zion.internal"
-  cn  = "R0"
-}
 
 module "k8s" {
   source = "./k8s"
@@ -11,3 +5,33 @@ module "k8s" {
   address = "s.unw.re"
   port    = 223
 }
+
+provider "kubernetes" {
+  host = module.k8s.endpoint
+
+  username               = module.k8s.config.users[0].name
+  cluster_ca_certificate = base64decode(module.k8s.config.clusters[0].cluster.certificate-authority-data)
+  client_certificate     = base64decode(module.k8s.config.users[0].user.client-certificate-data)
+  client_key             = base64decode(module.k8s.config.users[0].user.client-key-data)
+}
+
+provider "helm" {
+  kubernetes {
+    host = module.k8s.endpoint
+
+    username               = module.k8s.config.users[0].name
+    cluster_ca_certificate = base64decode(module.k8s.config.clusters[0].cluster.certificate-authority-data)
+    client_certificate     = base64decode(module.k8s.config.users[0].user.client-certificate-data)
+    client_key             = base64decode(module.k8s.config.users[0].user.client-key-data)
+  }
+}
+
+# The cluster's internal communication CA.
+module "ca" {
+  source = "./ca"
+
+  org = "k3s.zion.internal"
+  cn  = "R0"
+}
+
+
