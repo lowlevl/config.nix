9903cf0d0318100ea8e328c1147f50d0d8759700
Maya the bee <hello@unw.re>
2025-04-30 17:07:58 +0200

    security: change file masks for boot partition

--- machine/core/hardware-configuration.nix
+++ machine/core/hardware-configuration.nix
@@ -27,7 +27,7 @@
   fileSystems."/boot" = {
     device = "/dev/disk/by-uuid/87FA-C862";
     fsType = "vfat";
-    options = ["fmask=0022" "dmask=0022"];
+    options = ["fmask=0077" "dmask=0077"];
   };
 
   swapDevices = [
