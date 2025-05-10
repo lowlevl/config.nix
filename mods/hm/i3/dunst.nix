57b5b707e3ecd9b8780ac81b0f5a263f59b9588e
Maya the bee <hello@unw.re>
2025-05-10 20:10:11 +0200

    hm/i3: configure dunst colors

--- mods/hm/i3/dunst.nix
+++ mods/hm/i3/dunst.nix
@@ -1,5 +1,25 @@
 {...}: {
   services.dunst = {
     enable = true;
+
+    settings = {
+      global = {
+        width = 320;
+        offset = "32x48";
+
+        follow = "mouse";
+        origin = "top-right";
+        alignment = "left";
+
+        transparency = 4;
+
+        corner_radius = 4;
+        frame_color = "#161b22";
+        frame_width = 1;
+      };
+
+      urgency_normal.background = "#37474f";
+      urgency_critical.background = "#b04027";
+    };
   };
 }
