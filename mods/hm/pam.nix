0472b0d5b24d9a88275e4864448cc17092e7fe70
Maya the bee <hello@unw.re>
2025-05-11 13:14:38 +0200

    hm/pam: set the overlay to only override i3lock/i3lock-color

--- mods/hm/pam.nix
+++ mods/hm/pam.nix
@@ -1,16 +1,25 @@
-{pkgs, ...}: let
+{
+  pkgs,
+  lib,
+  ...
+}: let
   path = "/usr/bin/unix_chkpwd";
 
-  patch = pkgs.writeText "suid-wrapper-path.patch" ''
-    It needs the SUID version during runtime, and that can't be in /nix/store/**
-    --- a/modules/pam_unix/Makefile.am
-    +++ b/modules/pam_unix/Makefile.am
-    @@ -21 +21 @@
-    -	-DCHKPWD_HELPER=\"$(sbindir)/unix_chkpwd\" \
-    +	-DCHKPWD_HELPER=\"${path}\" \
-  '';
-  overlay = final: prev: {
-    linux-pam = prev.linux-pam.overrideAttrs (old: {patches = [patch];});
+  overlay = self: super: {
+    linux-pam-non-nix = let
+      patch = pkgs.writeText "suid-wrapper-path.patch" ''
+        It needs the SUID version during runtime, and that can't be in /nix/store/**
+        --- a/modules/pam_unix/Makefile.am
+        +++ b/modules/pam_unix/Makefile.am
+        @@ -21 +21 @@
+        -	-DCHKPWD_HELPER=\"$(sbindir)/unix_chkpwd\" \
+        +	-DCHKPWD_HELPER=\"${path}\" \
+      '';
+    in
+      super.linux-pam.overrideAttrs (old: {patches = [patch];});
+
+    i3lock = super.i3lock.override {pam = self.linux-pam-non-nix;};
+    i3lock-color = super.i3lock-color.override {pam = self.linux-pam-non-nix;};
   };
 in {
   nixpkgs.overlays = [overlay];
