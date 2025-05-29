b00c331c5511fb628a65e0c9223641bf03d159d0
Maya the bee <hello@unw.re>
2025-05-29 14:48:03 +0200

    hm/X: use new autorandr options and ditch the overlay

--- mods/hm/X/autorandr.nix
+++ mods/hm/X/autorandr.nix
@@ -2,14 +2,9 @@
   pkgs,
   lib,
   ...
-}: let
-  overlay = self: super: {
-    autorandr = pkgs.writeShellScriptBin "autorandr" "exec ${lib.getExe super.autorandr} --match-edid \"$@\"";
-  };
-in {
-  nixpkgs.overlays = [overlay];
-
+}: {
   services.autorandr = {
     enable = true;
+    matchEdid = true;
   };
 }
