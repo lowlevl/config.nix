50846137da38e6a66a3dfe5ffb179fab0c400c12
Maya the bee <hello@unw.re>
2025-06-24 21:35:30 +0200

    mods/users: add my phone's SSH key

--- mods/users.nix
+++ mods/users.nix
@@ -19,6 +19,7 @@
       openssh.authorizedKeys.keys = [
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIApk7ff1eG79YR6emIzR+iyXchjFlh1HdTK9Uki9YmN2 bee@hive"
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEac1v6+SJ9gZ208cyORTeD0U3/HcC5ovyvxJnH797o3 unix@socket"
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICdppi2v6SFIjXVrOvlPTEn3mqwhamM0b44xhgBM53pF phone:pixel8"
       ];
     };
   };
