2235e7baff3fce8fd42050a2bb4d302fdf2f63a5
Maya the bee <hello@unw.re>
2025-09-17 21:06:01 +0200

    confs/nyx: enable x86_64-linux execution

--- confs/nyx/default.nix
+++ confs/nyx/default.nix
@@ -3,6 +3,7 @@
   nixpkgs,
   sops-nix,
   pkgs,
+  lib,
   ...
 }: let
   pull-switch = pkgs.callPackage ../../pkgs/pull-switch.nix {};
@@ -37,14 +38,20 @@ in {
     ];
   };
 
-  # - Secrets configuration
-  sops.defaultSopsFile = ./secrets.yaml;
+  # - Enable x86_64 emulation
+  boot.binfmt = {
+    emulatedSystems = ["x86_64-linux"];
+    registrations.x86_64-linux.interpreter = lib.getExe pkgs.box64;
+  };
 
   # - Services and miscellaneous configuration
   networking.hostName = "nyx";
   networking.domain = "local";
 
   environment.systemPackages = [pull-switch];
+
+  # - Secrets configuration
+  sops.defaultSopsFile = ./secrets.yaml;
 
   # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
   system.stateVersion = "24.11"; # Did you read the comment?
