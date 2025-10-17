fbf07351e524bf70c14559afdb5cb0fec368ad39
Maya the bee <hello@unw.re>
2025-10-17 15:33:43 +0200

    confs/nyx: attempt to fix f2b

--- confs/nyx/security.nix
+++ confs/nyx/security.nix
@@ -1,18 +1,25 @@
 {...}: {
   networking.firewall.enable = true;
 
+  services.endlessh = {
+    enable = true;
+    port = 22;
+
+    openFirewall = true;
+  };
+
   services.fail2ban = {
     enable = true;
     maxretry = 5;
 
     bantime = "1h";
     bantime-increment.enable = true;
-  };
 
-  services.endlessh = {
-    enable = true;
-    port = 22;
-
-    openFirewall = true;
+    jails = {
+      sshd.settings = {
+        backend = "systemd";
+        mode = "aggressive";
+      };
+    };
   };
 }
